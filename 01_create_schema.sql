f503f258bb0a6596af3b0726eac10adfa5a206e6
Jazmin Villagra <jvillagra@sodep.com.py>
2019-03-16 20:21:36 -0300

    Agrega ejercicios 1, 8 y 9

--- 01_create_schema.sql
+++ 01_create_schema.sql
@@ -1,3 +1,116 @@
+-- 1) Ejercicio 1 del ejercitario
+
+select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'ALD_CONCEPTOS'
+ group by segment_name;
+
+
+select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'SOC_SOCIO'
+ group by segment_name;
+
+select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'ALD_INMUEBLES'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'ALD_DECLARACION_JURADA'
+ group by segment_name;
+
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'ALD_DETALLE_DECLARACION'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'SOC_OBLIGACIONES'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'SOC_DETALLE_OBLIGACIONES'
+ group by segment_name;
+
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'AHO_TIPO_MOVIMIENTO'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'AHO_CUENTA_AHORRO'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'AHO_MOVIMIENTOS_CUENTA'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'CRE_MODALIDAD_PRESTAMO'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'CRE_SOLICITUD_PRESTAMOS'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'CRE_PRESTAMOS'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'CRE_CUOTAS'
+ group by segment_name;
+
+ select segment_name table_name,       
+       sum(bytes)/(1024) table_size_bytes 
+  from user_extents 
+ where segment_type='TABLE' 
+   and segment_name = 'GEN_PARAMETROS'
+ group by segment_name;
+
+--para saber el tamaño de bloque para realizar los calculos
+ show parameters db_block_size
+ 
 -- 2) Deberá crear las tablas del esquema (con excepción de las marcadas en gris).
 -- Deberá considerar las claves primarias, constraints de no nulidad y claves
 -- foráneas. En este paso asigne por defecto el tablespace creado, pero no hace
