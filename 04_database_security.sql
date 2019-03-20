27bfdfc29cb13f93d47f9f9a1bdd1e6b775d0801
Jazmin Villagra <jvillagra@sodep.com.py>
2019-03-19 22:03:43 -0300

    ejercicio de seguridad actualizado

--- 04_database_security.sql
+++ 04_database_security.sql
@@ -5,28 +5,51 @@ IDENTIFIED BY jvillagra
 DEFAULT TABLESPACE BASEDATOS2
 TEMPORARY TABLESPACE tmp;
 
-GRANT CONNECT TO jazmin_villagra;
+GRANT CREATE SESSION TO jazmin_villagra;
+
+CREATE USER matias_fare
+IDENTIFIED BY mfare
+DEFAULT TABLESPACE BASEDATOS2
+TEMPORARY TABLESPACE tmp;
+
+GRANT CREATE SESSION TO matias_fare;
+
+CREATE USER yesshua_lopez
+IDENTIFIED BY ylopez
+DEFAULT TABLESPACE BASEDATOS2
+TEMPORARY TABLESPACE tmp;
+
+GRANT CREATE SESSION TO yesshua_lopez;
+
 COMMIT;
 -- 11)Cree un script para grantear al usuario creado los privilegios de:
 -- * SELECT sobre las tablas de los todos los módulos.
 -- * INSERT, UPDATE, DELETE sobre las tablas que pertenecen a los módulos
 -- de Solidaridad (SOL) y Créditos (CRE).
 
-spool /opt/oracle/oradata/grant-select-all.sql
+set pagesize 80;
+set linesize 180;
+column TABLE_NAME format A20; 
+SET HEADING OFF  FEEDBACK OFF ECHO OFF UNDERLINE OFF;
 
-SELECT 'GRANT SELECT ON '|| TABLE_NAME ||' TO jazmin_villagra'
+SELECT 'GRANT CREATE SESSION ON ' || TABLE_NAME || ' TO jazmin_villagra, matias_fare, yesshua_lopez;'
 FROM tabs;
 
+spool /opt/oracle/oradata/grant-select-all.sql
+/
 spool off
 
 COMMIT;
 
+set pagesize 80;
+set linesize 180;
+
+SELECT 'GRANT INSERT, UPDATE, DELETE ON ' || TABLE_NAME || ' TO jazmin_villagra, matias_fare, yesshua_lopez;'
+FROM tabs WHERE TABLE_NAME LIKE '%CRE%' OR TABLE_NAME LIKE '%SOL%';
+
+
 spool /opt/oracle/oradata/grant.sql
-
-SELECT 'GRANT INSERT, UPDATE, DELETE ON '|| TABLE_NAME ||' TO jazmin_villagra'
-FROM tabs
-WHERE TABLE_NAME LIKE '%CRE%' OR LIKE '%SOL%';
-
+/
 spool off
 
 COMMIT;
