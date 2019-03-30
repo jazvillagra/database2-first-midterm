2bfce9e669a32766535f354202d7d250005e6070
matiasfare <matiasfare59@gmail.com>
2019-03-30 11:11:03 -0400

    Ejer7 compatible con Oracle 12c

--- ejercicios-temporales/ejer07.sql
+++ ejercicios-temporales/ejer07.sql
@@ -25,3 +25,32 @@ ON  CA.ID_CUENTA = R.ID_CUENTA)
 WHEN matched THEN
 	UPDATE SET CA.SALDO_DIPONIBLE = R.RESTA
 ;
+
+--ESTE SIN FUNCIONA(a la version 12c no le gusta la mayuscula en el merge)
+merge into aho_cuenta_ahorro ca
+using (
+select ca.id_cuenta, 
+(select sum(mc.importe)
+from aho_cuenta_ahorro ca2 join aho_movimientos_cuenta mc
+on ca2.id_cuenta = mc.id_cuenta
+JOIN AHO_TIPO_MOVIMIENTO atm
+ON mc.ID_TIPO = atm.ID_TIPO
+where atm.debito_credito='C'and ca.id_cuenta = ca2.id_cuenta) 
+-
+(select sum(mc.importe)+ca2.saldo_bloqueado
+from aho_cuenta_ahorro ca2 join aho_movimientos_cuenta mc
+on ca2.id_cuenta = mc.id_cuenta
+JOIN AHO_TIPO_MOVIMIENTO atm
+ON mc.ID_TIPO = atm.ID_TIPO
+where atm.debito_credito='D' and ca.id_cuenta = ca2.id_cuenta
+group by ca2.id_cuenta, ca2.saldo_bloqueado) Resta
+from aho_cuenta_ahorro ca join aho_movimientos_cuenta mc
+on ca.id_cuenta = mc.id_cuenta
+JOIN AHO_TIPO_MOVIMIENTO atm
+ON mc.ID_TIPO = atm.ID_TIPO
+group by ca.id_cuenta
+) Resul
+on (ca.id_cuenta = Resul.id_cuenta)
+when matched then
+	update set ca.saldo_disponible = Resul.Resta
+;
